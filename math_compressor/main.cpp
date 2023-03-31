fe43583971aad0f8e11356b74232575b365fe767
nick <nick.notaweeb@gmail.com>
2023-03-31 22:08:40 +0100

    input

--- math_compressor/main.cpp
+++ math_compressor/main.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <iostream>
+#include <string>
 
 #include "Matrix.h"
 
@@ -19,24 +20,36 @@ long double f(long double x) {
 
 long double f(Matrix<long double> m, long double x) {
 	long double ret = 0;
+	static bool done = false;
+
+	std::string equation = "y = ";
 
 	for (int i = 0; i < m.rows(); i++) {
+		if (!done)
+			equation.append(std::to_string(m(i, 0)) + std::string("x^{") + std::to_string(i) + std::string("} + "));
 		ret += (m(i, 0) * pow(x, i));
 	}
+
+	if (!done)
+		printf("equation : %s\n", equation.c_str());
+	done = true;
 
 	return ret;
 }
 
 int main() {
-	int* msg = new int[8];
+	printf("input message to be compressed: ");
+	std::string inp;
+	std::getline(std::cin, inp);
 
+	/*int* msg = new int[8];
 
 	for (int i = 1; i <= 8; i++) {
 		msg[i - 1] = std::round(f(i));
 		printf("[%d] %d => %c\n", i, msg[i - 1], (char)msg[i - 1]);
-	}
+	}*/
 
-	Matrix<long double> m1(8,8);
+	Matrix<long double> m1(inp.size(), inp.size());
 	for (int i = 0; i < m1.rows(); i++) {
 		for (int j = 0; j < m1.cols(); j++) {
 			m1(i, j) = pow(i + 1, j);
@@ -61,15 +74,19 @@ int main() {
 		std::cout << "\n";
 	}*/
 
-	Matrix<long double> b(8, 1);
-	b(0, 0) = 98;
+	Matrix<long double> b(inp.size(), 1);
+	for (int i = 0; i < inp.size(); i++) {
+		printf("%d\n", inp[i]);
+		b(i, 0) = (char)inp[i];
+	}
+	/*b(0, 0) = 98;
 	b(1, 0) = 111;
 	b(2, 0) = 111;
 	b(3, 0) = 107;
 	b(4, 0) = 108;
 	b(5, 0) = 101;
 	b(6, 0) = 116;
-	b(7, 0) = 115;
+	b(7, 0) = 115;*/
 	//b(8, 0) = 111;
 	//b(9, 0) = 111;
 
@@ -81,9 +98,9 @@ int main() {
 		std::cout << "\n";
 	}*/
 
-	for (int i = 1; i <= 8; i++) {
-		msg[i - 1] = std::round(f(matrix_X, i));
-		printf("[%d] %d => %c\n", i, msg[i - 1], (char)msg[i - 1]);
+	for (int i = 1; i <= inp.size(); i++) {
+		int n = std::round(f(matrix_X, i));
+		printf("[%d] %d => %c\n", i, n, (char)n);
 	}
 
 	//for (int i = 0; i < 8; i++) {
